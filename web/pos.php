5b33dbb3eb0a7ecfe624b3352f1a27fadfb3c316
Yukio Yamamoto <VZC02113@nifty.com>
2012-04-21 23:13:05 +0900

    Add web service api to get planetary positions

---
+++ web/pos.php
@@ -0,0 +1,27 @@
+<?php
+
+$input = array();
+foreach($_GET as $key => $val) {
+  $input[$key] = escapeshellarg($val);
+}
+
+$datetime = $input['datetime'];
+$lat = $input['lat'];
+$lon = $input['lon'];
+
+$cmd = "/usr/local/bin/earth_info isac.meta $datetime $lat $lon";
+exec($cmd,$output,$ret);
+
+foreach($output as $line) {
+  $data = explode(',',$line);
+  echo data2json($data) . "\n";
+}
+
+function data2json($data) {
+  $str = sprintf('{"%s":[%.2f,%.2f,%.2f]}',
+		 str_replace(' BARYCENTER', '', $data[0]),
+		 $data[1],
+		 $data[2],
+		 $data[3]);
+  return $str;
+}
